7c1e0cab23c4dc4b137e79988aca85b06c957905
nadimgkmit <nadim@gkmit.co>
2020-04-28 16:39:59 +0530

    setup react bootstrap #172241441

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './component/header';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
