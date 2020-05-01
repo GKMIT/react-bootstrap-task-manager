eec77b4627619d286d3cd7b66c5380eab4e26ac4
nadimgkmit <nadim@gkmit.co>
2020-05-01 10:49:44 +0530

    add sweetalert for alert & confirm  [#172242160]

--- src/component/alert/alertConfirmDialog.jsx
+++ src/component/alert/alertConfirmDialog.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
+import SweetAlert from 'react-bootstrap-sweetalert';
 
 export default function AlertConfirmDialog(props) {
     return (
-        <div>
-            {/* <Dialog
-                open={props.open}
-                onClose={props.handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">{props.title}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        {props.text}
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={props.handleClose} color="primary">No</Button>
-                    <Button onClick={props.handleConfirm} color="primary" autoFocus>Yes</Button>
-                </DialogActions>
-            </Dialog> */}
-        </div>
+        <SweetAlert
+            warning
+            showCancel
+            title={props.title}
+            onConfirm={props.handleConfirm}
+            onCancel={props.handleClose}
+            confirmBtnText="Yes"
+            cancelBtnText="No"
+            confirmBtnBsStyle="primary"
+            cancelBtnBsStyle="default"
+        >
+            {props.text}
+        </SweetAlert>
     );
 }
