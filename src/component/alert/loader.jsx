01ab83ec61829ca82632da5fda06cf3605f95b2d
nadimgkmit <nadim@gkmit.co>
2020-05-12 15:34:13 +0530

    add loader [#172241373]

--- src/component/alert/loader.jsx
+++ src/component/alert/loader.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Spinner } from 'react-bootstrap';
+import { Modal, Spinner } from 'react-bootstrap';
 
 export default function Loader(props) {
     if (props.open) {
         return (
-            <Spinner animation="border" role="status">
-                <span className="sr-only">Loading...</span>
-            </Spinner>
+            <Modal show={true} centered className="modal-loader">
+                <Modal.Body>
+                    <Spinner animation="border" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </Spinner>
+                    <p>Loading...</p>
+                </Modal.Body>
+            </Modal>
         )
     }
     return ""
